1f66f4be8b5a5ffadc1bfc617799c13f17536bf6
zhangjiadong <zhangjiadong@jkd.com>
2020-12-08 16:18:12 +0800

    ing

--- cloud-entity/src/main/java/utils/RestUtils.java
+++ cloud-entity/src/main/java/utils/RestUtils.java
@@ -1,11 +1,23 @@
 package utils;
 
+import com.alibaba.fastjson.JSON;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
 /**
  * @author zjd
  * @date 2020/12/7
  */
+@Configuration
 public class RestUtils {
+
+    private static  RestTemplate restTemplate = new RestTemplate();
+
+    public static  <T> T  getForEntity(String url,Class<T> clazz){
+        return JSON.parseObject(JSON.toJSONString(restTemplate.getForObject(url, Object.class)), clazz);
+    }
+
 
 }
--- cloud-order-7000/src/main/java/com/zzz/cloud/controller/OrderController.java
+++ cloud-order-7000/src/main/java/com/zzz/cloud/controller/OrderController.java
@@ -1,9 +1,13 @@
 package com.zzz.cloud.controller;
 
+import com.alibaba.fastjson.JSON;
 import entity.BackMessage;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import utils.RestUtils;
 
 /**
  * @author zjd
@@ -14,4 +18,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class OrderController {
 
 
+
+    @GetMapping("/getOne")
+    public BackMessage getOnePayment(Integer id){
+        BackMessage<?> message = RestUtils.getForEntity("http://localhost:7001/pay/payment?id=" + id, BackMessage.class);
+        return message;
+    }
 }
