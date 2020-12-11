ace1be1a49249290cb0c4d60be55999d8f6c4471
zjd1135715246 <1135715246@qq.com>
2020-12-11 21:02:48 +0800

    ing

--- cloud-p-hystrix-7001/src/main/java/com/zzz/cloud/PHystrixMain.java
+++ cloud-p-hystrix-7001/src/main/java/com/zzz/cloud/PHystrixMain.java
@@ -2,12 +2,16 @@ package com.zzz.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author zjd
  * @date 2020/12/10
  */
 @SpringBootApplication
+@EnableEurekaClient
+@EnableCircuitBreaker
 public class PHystrixMain {
 
     public static void main(String[] args) {
--- cloud-p-hystrix-7001/src/main/java/com/zzz/cloud/service/impl/PaymentServiceImpl.java
+++ cloud-p-hystrix-7001/src/main/java/com/zzz/cloud/service/impl/PaymentServiceImpl.java
@@ -1,5 +1,7 @@
 package com.zzz.cloud.service.impl;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import com.zzz.cloud.service.PaymentService;
 import entity.BackMessage;
 import org.springframework.stereotype.Service;
@@ -23,18 +25,28 @@ public class PaymentServiceImpl implements PaymentService {
     }
 
     @Override
+    @HystrixCommand(fallbackMethod = "getOneSleepHandler",commandProperties = {
+       @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds",value = "2000")
+    })
     public BackMessage getOneSleep(Integer id) {
         BackMessage message = new BackMessage();
-
-        try {
+        int x = 10/0;
+        /*try {
             Thread.sleep(3000);
         }catch (Exception e){
             e.printStackTrace();
-        }
+        }*/
         message.setCode(200);
         message.setMessage("执行了3s,当前线程id："+Thread.currentThread().getName()+"---"+id);
 
 
+        return message;
+    }
+
+    public BackMessage getOneSleepHandler(Integer id) {
+        BackMessage message = new BackMessage();
+        message.setCode(400);
+        message.setMessage("有点问题");
         return message;
     }
 }
